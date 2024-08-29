3fbe18f51fd66f39d2a41a0f8648c73eb35809a0
Sparkoms <sparkoms@gmail.com>
2024-08-29 16:55:56 +0300

    cards

---
+++ src/Movies.js
@@ -0,0 +1,12 @@
+function Movies() {
+    return (
+        <article>
+            <header>
+                <h1>Список фильмов</h1>
+            </header>
+            <div className="movies">
+                
+            </div>
+        </article>
+    );
+}
