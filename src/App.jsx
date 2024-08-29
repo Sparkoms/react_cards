7f0758a6878774e92676782a50abca87623f1901
Sparkoms <sparkoms@gmail.com>
2024-08-29 14:50:48 +0300

    first cards

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient();
+
+function App() {
+
+
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div />
+    </QueryClientProvider>
+  )
+}
+
+export default App
